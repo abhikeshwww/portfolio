8ad23636fd0bb705445cac4506f9138535d13aad
abhikeshwww <137013271+abhikeshwww@users.noreply.github.com>
2023-07-28 16:41:50 +0900

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { Provider } from 'react-redux';
+import './style.css';
+import store from './Utils/store';
+import Head from './Components/Head';
+import Body from './Components/Body';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Head />
+        <Body />
+      </div>
+    </Provider>
   );
 }
